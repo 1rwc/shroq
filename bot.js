eda0ee3f6b8086119d7b2fad8c0c3b88b902b331
1rwc <46110346+1rwc@users.noreply.github.com>
2019-01-17 19:36:16 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -226,7 +226,7 @@ function play(guild, song) {
         .on('error', error => console.error(error));
     dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
  
-    serverQueue.textChannel.send(` ``🎵 بدء تشغيل``  : ``${song.title}`` `);
+    serverQueue.textChannel.send(` ``🎵 بدء تشغيل``  : **${song.title}**`);
 }
  
  

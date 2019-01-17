10a0131c67cccb4aecbe869d50952425619546cf
1rwc <46110346+1rwc@users.noreply.github.com>
2019-01-17 19:45:32 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -126,7 +126,7 @@ ${videos.map(video2 => `[**${++index} **] \`${video2.title}\``).join('\n')}`)
     } else if (command === `vol`) {
         if (!msg.member.voiceChannel) return msg.channel.send('أنت لست بروم صوتي .');
         if (!serverQueue) return msg.channel.send('لا يوجد شيء شغآل.');
-        if (!args[1]) return msg.channel.send(`:loud_sound: مستوى الصوت **${serverQueue.volume}**`);
+        if (!args[1]) return msg.channel.send(` `` :loud_sound: مستوى الصوت `` **${serverQueue.volume}**`);
         serverQueue.volume = args[1];
         serverQueue.connection.dispatcher.setVolumeLogarithmic(args[1] / 50);
         return msg.channel.send(`:speaker: تم تغير الصوت الي **${args[1]}**`);
